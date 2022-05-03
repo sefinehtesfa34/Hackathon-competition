02f820b6891563271350f9111c73cd36bff30155
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-05-03 22:27:14 +0300

    third commit

--- connector/views.py
+++ connector/views.py
@@ -1,5 +1,6 @@
 
 import os
+from zipfile import ZipFile
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
@@ -22,14 +23,28 @@ def upload(request):
     
     if request.method=='POST':
         uploaded_file=request.FILES['file-name']
-        print(request.POST["job-Description"])
+        # print(request.POST["job-Description"])
         fs=FileSystemStorage()
-        
+        file_name_truncated=str(uploaded_file.name).split('.')[0]
+        # print("uploaded file name: ",uploaded_file.name)        
         fs.save(uploaded_file.name,uploaded_file)
         file_path=os.path.join(settings.MEDIA_ROOT,uploaded_file.name)
-        cleaned_resume=resumeExtractor(file_path=file_path)
-        index = model.predict(tf.convert_to_tensor([cleaned_resume])).argmax()
-        predicted=dict_model[index]
+        output=[]
+        resume_name=[]
+        with ZipFile(file_path,'r') as zpfile:
+            zpfile.extractall('./extracted')
+            for file_name in os.listdir(os.path.join("./extracted",file_name_truncated)):
+                file_path=os.path.join(os.path.join('./extracted',file_name_truncated),file_name)
+                cleaned_resume=resumeExtractor(file_path=file_path)
+                index = model.predict(tf.convert_to_tensor([cleaned_resume])).argmax()
+                predicted=[dict_model[index]]
+                output.extend(predicted)
+                resume_name.append(file_name)
+                os.remove(file_path)
+                
+        os.rmdir(os.path.join("./extracted",file_name_truncated))
+        predicted=list(zip(output,resume_name))
+        os.remove(f"./media/{uploaded_file.name}")
     return render(request,'connector/home.html',context={'predicted':predicted})
 
     
@@ -47,7 +62,7 @@ def register(request):
         return render(request,'connector/register.html')
     
 def login(request):
-    
+
     return render(request,'connector/login.html')
 
     
